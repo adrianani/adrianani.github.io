add8e948ab9f6491fef30a551d598bf2ea2429ec
Adrian Ani <30348211+adrianani@users.noreply.github.com>
2020-03-13 18:23:22 +0200

    v1.0

---
+++ precache-manifest.29a1b78ed8c36fcf7f3431b8544b4913.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c589fbd3c75ce5887c46",
+    "url": "/css/app.0a255ae1.css"
+  },
+  {
+    "revision": "a7fbcf14dc4003625710c19dbf455563",
+    "url": "/img/automatic-social-promotion.a7fbcf14.png"
+  },
+  {
+    "revision": "1435c5c5edb1126cea6a9957009066eb",
+    "url": "/img/avatar.1435c5c5.jpg"
+  },
+  {
+    "revision": "69759c984cd6aa771d6cdb6c76e3f09d",
+    "url": "/img/forum-custom-reactions.69759c98.png"
+  },
+  {
+    "revision": "746d12de164143ebab6d96455fc99cd1",
+    "url": "/img/hidden-attachments.746d12de.png"
+  },
+  {
+    "revision": "322937759158a84111dc5ebb98d0c30f",
+    "url": "/img/sidebar-discord-server.32293775.png"
+  },
+  {
+    "revision": "3107fab6da340fed8b42c16ff435d954",
+    "url": "/img/sidebar-steam-group.3107fab6.png"
+  },
+  {
+    "revision": "0a3313a4c91672b95de829f20a9eb987",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c589fbd3c75ce5887c46",
+    "url": "/js/app.ebe48490.js"
+  },
+  {
+    "revision": "edd60fe78afeb3a95b9f",
+    "url": "/js/chunk-vendors.2608f2bb.js"
+  },
+  {
+    "revision": "c23a909ce1cf0ab88584397c034e9d5c",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
